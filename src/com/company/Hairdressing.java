5734da49636e5f74122eebb8d13ffa6e53545905
AndreiBy <andreialshuk@gmail.com>
2020-03-19 14:30:26 +0300

    Initial commit

---
+++ src/com/company/Hairdressing.java
@@ -0,0 +1,6 @@
+package com.company;
+
+public class Hairdressing {
+    String tep;
+
+}
---
+++ src/com/company/Services.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Services {
+    String date;
+    String master;
+
+
+
+}
